20e94467d67e51f009c6d8ea84fcf8c47e3ceb93
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-09-14 21:46:17 -0300

    Se agrego el contenedor de la calculadora y su css correspondiente

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div className='App'>
+      <div className='titulo'>
+        <h1>
+          Calculadora
+        </h1>
+      </div>
+      <div className='contenedor-calculadora'>
+
+      </div>
     </div>
   );
 }
