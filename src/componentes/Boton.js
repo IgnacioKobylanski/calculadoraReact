d7b3d7eb5849bf36aea711ca55f69fc144d42e56
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2023-09-18 20:42:24 -0300

    se agrego el boton de reset y la funcionalidad de la calculadora instalando la dependencia mathjs

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -7,7 +7,7 @@ function Boton(props) {
   };
 
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`}>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`} onClick={() => props.manejarClic(props.children)} >
       {props.children}
     </div>
   );
